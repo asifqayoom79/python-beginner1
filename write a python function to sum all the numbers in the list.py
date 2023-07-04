06f3b44bcf6abe17e66729c62c7896fb1e1dc05d
asifqayoom79 <136574551+asifqayoom79@users.noreply.github.com>
2023-07-04 00:02:42 -0700

    write a python function to sum all the numbers in the list.py

--- write a python function to sum all the numbers in the list.py
+++ write a python function to sum all the numbers in the list.py
@@ -1,6 +1,9 @@
 def sum_numbers(list1):
+    
     total = sum(list1)
+    
     return total
+    
 list1 = list(map(int,input("enter the list:").split()))
 result = sum_numbers(list1)
 print("Sum of numbers:", result)
