c5ac272a21182d8a6d6071b33c6a7d18f401879b
asifqayoom79 <136574551+asifqayoom79@users.noreply.github.com>
2023-07-21 18:50:33 +0530

    Add files via upload

---
+++ employees.py
@@ -0,0 +1,30 @@
+import json
+
+class Employee:
+    def __init__(self, name, dob, height, city, state):
+        self.name = name
+        self.dob = dob
+        self.height = height
+        self.city = city
+        self.state = state
+
+with open("C:/Users/Asif Qayom/Desktop/employee.json") as file:
+    data = json.load(file)
+
+employee_list = []
+for employee_data in data["employees"]:
+    name = employee_data["Name"]
+    dob = employee_data["DOB"]
+    height = employee_data["Height"]
+    city = employee_data["City"]
+    state = employee_data["State"]
+    employee = Employee(name, dob, height, city, state)
+    employee_list.append(employee)
+
+for employee in employee_list:
+    print("Name:", employee.name)
+    print("DOB:",employee.dob)
+    print("Height:",employee.height)
+    print("City:",employee.city)
+    print("State:",employee.state)
+    print()
