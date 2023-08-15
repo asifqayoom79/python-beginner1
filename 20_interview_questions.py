1bf436a7023f3140933f4f70c917d7c7684d7020
asifqayoom79 <136574551+asifqayoom79@users.noreply.github.com>
2023-08-15 19:05:47 +0530

    Add files via upload

---
+++ 20_interview_questions.py
@@ -0,0 +1,58 @@
+#   Data Type Questions Asked in interview are:
+#Q.01) What are the fundamental data types in Python?
+#Q.02) Explain the concept of dynamic typing in Python.
+#Q.03) Differentiate between mutable and immutable data types with examples.
+#Q.04) How can you check the type of a variable using a built-in function?
+#Q.05) What are the differences between a list and a tuple?
+#Q.06) What is the purpose of dictionaries in Python?
+#Q.07) Explain the significance of the None value and its data type.
+#Q.08) What is type casting, and how can you perform it in Python?
+#Q.09) How does Python handle complex numbers?
+#Q.10) What are the characteristics of sets and frozensets in Python?
+
+#   Operators:
+#Q.01) Explain the role of the + operator with strings and numbers.
+#Q.02) What is the modulus operator % used for?
+#Q.03) Describe the behavior of the and and or logical operators.
+#Q.04) What does the not operator do?
+#Q.05) How can you compare the identity of two objects in Python?
+#Q.06) Explain the difference between = and == in Python.
+#Q.07) What are the logical operators in Python and how are they used?
+#Q.08) Describe the role of the bitwise operators.
+#Q.09) How does operator precedence work in Python expressions?
+#Q.10) Explain the concept of operator overloading with examples.
+# Conditional Statements:
+#Q.01) How is an if statement used in Python?
+#Q.02) What is the purpose of an elif statement?
+#Q.03) How do you use the else statement in a conditional structure?
+#Q.04) Explain the concept of nested conditional statements.
+#Q.05) How can you simulate a switch-case statement in Python?
+#Q.06) How can you simulate a switch-case statement in Python?
+#Q.07) Describe the assert statement and when it is used.
+#Q.08) Explain the try, except, else, and finally blocks in exception handling.
+#Q.09) How can you use the ternary operator for conditional expressions?
+#Q.10) What is the difference between == and is when comparing variables?
+
+# Looping Statements:
+#Q.01) How is a for loop utilized in Python?
+#Q.02) Describe the functionality of the range() function in a for loop.
+#Q.03) Describe the for loop and provide examples of its usage.
+#Q.04) How do you use the range() function in a for loop?
+#Q.05) Explain the while loop and how it works.
+#Q.06) What is the purpose of the break and continue statements?
+#Q.07) How can you iterate over dictionaries using a for loop?
+#Q.08) Explain the concept of an infinite loop and how to avoid it.
+#Q.09) What is the enumerate() function used for in looping?
+#Q.10) Describe the zip() function and its significance.
+
+# Functions:
+#Q.01) What is a function in Python? Provide the syntax of function definition.
+#Q.02) How do you call a function and pass arguments to it?
+#Q.03) Explain the purpose of the return statement in a function.
+#Q.04) What are default arguments in a function, and how do you use them?
+#Q.05) Describe the concept of variable scope in relation to functions.
+#Q.06) How can you define a lambda function, and where is it commonly used?
+#Q.07) Explain the difference between a function and a method.
+#Q.08) What is recursion, and why might you use it in a function?
+#Q.09) How do you pass a variable number of arguments to a function?
+#Q.10) What are docstrings, and why are they used in functions?
